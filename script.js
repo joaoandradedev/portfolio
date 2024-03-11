326f45e6820d7fa848672a20d73a3427854fb11c
João Vitor <joaovitor2004andrade@gmail.com>
2024-03-10 23:12:08 -0300

    alteraçao

--- script.js
+++ script.js
@@ -3,16 +3,3 @@ const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
 
-window.onscroll = function() {mostrarImagem()};
-
-function mostrarImagem() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("minhaImagem").style.display = "block";
-  } else {
-    document.getElementById("minhaImagem").style.display = "none";
-  }
-}
-
-document.getElementById("minhaImagem").onclick = function() {
-  document.documentElement.scrollTop = 0;
-}
